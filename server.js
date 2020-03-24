2e646fecd0433dea98416ded1f86f2ebaebbe4c2
Suraj Gawas <suraj.gawas@nike.com>
2020-03-24 00:38:24 -0700

    second commit

--- server.js
+++ server.js
@@ -42,8 +42,10 @@ app.get('/profile/:id', (req, res) =>{ profile.handleProfileGet(req, res, db)})
 app.put('/image', (req,res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)});
 
-app.listen(3000, ()=>{
-    console.log('Listening on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`);
 })
 
 // const saltRounds = 10 // increase this if you want more iterations  
